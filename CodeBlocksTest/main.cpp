87fbc3342a8046d6b39809499b8b18fb5951fa24
armagedescu <armagedescu@gmail.com>
2020-09-06 21:50:01 +0300

    Codeblock first

---
+++ CodeBlocksTest/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void dosalut()
+{
+   cout << "salut"<< endl;
+}
+void donoroc();
+int main()
+{
+    donoroc();
+    int i = 0;
+    i++;
+    cout << "Hello world! " << i<< endl;
+    return 0;
+}
---
+++ CodeBlocksTest/noroc.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void donoroc()
+{
+   cout << "noroc"<< endl;
+}
