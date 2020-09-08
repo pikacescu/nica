f0f56e3d0fc8025bfdf3978516aaaaf1210df56d
pikacescu <nicoleta.filipski@gmail.com>
2020-09-08 12:08:42 +0300

    Primul proiect

---
+++ cpp/first/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+//first C++ program
+
+using namespace std;
+
+int main()
+{
+    cout << "Filipski Nica" <<endl;
+    cout <<"\n Cazacu Doina - info teacher";
+    cout <<"\n\t e-mail:doinagriu22@gmail.com"<<endl;
+    cout <<endl;
+    cout <<25<<endl;
+    //cout <<"hello"<<endl;
+
+    string str="hello";
+    cout <<str<<endl;
+    cout<<endl<<endl;
+
+    int x; int y;
+    //cout <<"insert any value for x" <<endl;
+    //cin >>x;
+    //cout <<"insert any value for y" <<endl;
+    //cin>> y;
+    //cout << "value of x = " << x<< endl;
+    //cout << "value of y = " << y<< endl;
+    //cout<< "sum of x and y; x + y = "<< x + y<< endl;
+
+    float a, b;
+    cout <<"insert a" <<endl;
+    cin >>a;
+    cout <<"incert b" <<endl;
+    cin >>b;
+    cout <<a / b;
+
+    return 0;
+}
