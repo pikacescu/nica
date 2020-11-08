66e3a251a2a6448d66bc1cd71f20298dd09b0966
armagedescu <armagedescu@gmail.com>
2020-11-08 20:19:00 +0200

    rework

--- cpp/proiectnou/main.cpp
+++ cpp/proiectnou/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Programarea structurata este do disciplina care se invata in anul intai." << endl;
+    cout << "Filipski Nicoleta P-2011" <<endl;
+
     return 0;
 }
